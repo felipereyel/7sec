2d1061f2ec25b1a03e0c9a316a24cf4150decd1e
felipereyel <felipe.reyel@gmail.com>
2024-07-21 18:36:10 -0300

    start pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VitePWA({ registerType: 'autoUpdate' }),
     vue(),
+    VitePWA({ 
+      includeAssets: ['icon.svg', 'icon-small.svg'],
+      manifest: {
+        name: '7sec',
+        short_name: '7sec',
+        description: '7 seconds to clear them all',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-small.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: 'icon.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      }
+    }),
   ],
   server: {
     host: "0.0.0.0"
